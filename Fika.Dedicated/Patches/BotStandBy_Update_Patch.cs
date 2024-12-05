360b34ccd0702fee0198881a3591523bfba294d3
Archangel <jesse@archangel.wtf>
2024-12-06 00:49:30 +0100

    Disable bot sleeping by default (Add option to re-enable it)

---
+++ Fika.Dedicated/Patches/BotStandBy_Update_Patch.cs
@@ -0,0 +1,20 @@
+﻿using SPT.Reflection.Patching;
+using System.Reflection;
+
+namespace Fika.Dedicated.Patches
+{
+	// The purpose of this patch is to disable bot sleeping on the dedicated host.
+	internal class BotStandBy_Update_Patch : ModulePatch
+	{
+		protected override MethodBase GetTargetMethod()
+		{
+			return typeof(BotStandBy).GetMethod(nameof(BotStandBy.Update));
+		}
+
+		[PatchPrefix]
+		public static bool Prefix()
+		{
+			return false;
+		}
+	}
+}

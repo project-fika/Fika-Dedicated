765831edcc3ad3d664621cb733fe66eafb59ca34
Lacyway <20912169+Lacyway@users.noreply.github.com>
2024-12-06 09:28:15 +0100

    Update doc for patch

--- Fika.Dedicated/Patches/BotStandBy_Update_Patch.cs
+++ Fika.Dedicated/Patches/BotStandBy_Update_Patch.cs
@@ -3,7 +3,9 @@ using System.Reflection;
 
 namespace Fika.Dedicated.Patches
 {
-	// The purpose of this patch is to disable bot sleeping on the dedicated host.
+	/// <summary>
+	/// The purpose of this patch is to disable bot sleeping on the dedicated host
+	/// </summary>
 	internal class BotStandBy_Update_Patch : ModulePatch
 	{
 		protected override MethodBase GetTargetMethod()

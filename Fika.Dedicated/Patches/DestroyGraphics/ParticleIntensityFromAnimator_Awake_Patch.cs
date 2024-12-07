865c12908517d5d32aa49fc9f75fe751bc54e4d1
Lacyway <20912169+Lacyway@users.noreply.github.com>
2024-12-07 22:06:22 +0100

    Add ParticleIntensityFromAnimator_Awake_Patch

---
+++ Fika.Dedicated/Patches/DestroyGraphics/ParticleIntensityFromAnimator_Awake_Patch.cs
@@ -0,0 +1,22 @@
+﻿using EFT;
+using SPT.Reflection.Patching;
+using System.Reflection;
+using UnityEngine;
+
+namespace Fika.Dedicated.Patches.DestroyGraphics
+{
+	internal class ParticleIntensityFromAnimator_Awake_Patch : ModulePatch
+	{
+		protected override MethodBase GetTargetMethod()
+		{
+			return typeof(ParticleIntensityFromAnimator).GetMethod(nameof(ParticleIntensityFromAnimator.Awake));
+		}
+
+		[PatchPrefix]
+		public static bool Prefix(ParticleIntensityFromAnimator __instance)
+		{
+			GameObject.Destroy(__instance.gameObject);
+			return false;
+		}
+	}
+}

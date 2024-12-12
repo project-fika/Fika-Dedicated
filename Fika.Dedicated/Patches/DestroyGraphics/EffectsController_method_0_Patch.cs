a0b1790f25f2cdc8d333e91123898f26d6b9d8dd
Lacyway <20912169+Lacyway@users.noreply.github.com>
2024-12-12 10:07:35 +0100

    Fix errors on effectscontroller

---
+++ Fika.Dedicated/Patches/DestroyGraphics/EffectsController_method_0_Patch.cs
@@ -0,0 +1,22 @@
+﻿using EFT;
+using EFT.CameraControl;
+using SPT.Reflection.Patching;
+using System.Reflection;
+
+namespace Fika.Dedicated.Patches.DestroyGraphics
+{
+	internal class EffectsController_method_0_Patch : ModulePatch
+	{
+		protected override MethodBase GetTargetMethod()
+		{
+			return typeof(EffectsController).GetMethod(nameof(EffectsController.method_0));
+		}
+
+		[PatchPrefix]
+		public static bool Prefix(ref Player ___player_0, PlayerCameraController playerCameraController)
+		{
+			___player_0 = playerCameraController.Player;
+			return false;
+		}
+	}
+}

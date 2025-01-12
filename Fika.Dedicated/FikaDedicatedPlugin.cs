a0d404f3f3199229197fec35b929d22afdc8c5f3
Lacyway <20912169+Lacyway@users.noreply.github.com>
2025-01-12 10:31:40 +0100

    Update references and bump version

--- Fika.Dedicated/FikaDedicatedPlugin.cs
+++ Fika.Dedicated/FikaDedicatedPlugin.cs
@@ -42,7 +42,7 @@ namespace Fika.Dedicated
 	[BepInDependency("com.SPT.custom", BepInDependency.DependencyFlags.HardDependency)]
 	public class FikaDedicatedPlugin : BaseUnityPlugin
 	{
-		public const string DediVersion = "1.2.3";
+		public const string DediVersion = "1.2.4";
 
 		public static FikaDedicatedPlugin Instance { get; private set; }
 		public static ManualLogSource FikaDedicatedLogger;

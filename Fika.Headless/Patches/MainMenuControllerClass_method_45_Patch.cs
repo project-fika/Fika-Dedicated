79b041b4e21e2f190908378bcb31ac6f946ed89a
Archangel <jesse@archangel.wtf>
2025-01-17 15:19:37 +0100

    Prep for new SPT mappings

---
+++ Fika.Headless/Patches/MainMenuControllerClass_method_45_Patch.cs
@@ -0,0 +1,23 @@
+﻿using SPT.Reflection.Patching;
+using System.Reflection;
+
+namespace Fika.Headless.Patches
+{
+    /// <summary>
+    /// This patch simulates clicking "Next" by calling the method bound to the event of the button
+    /// </summary>
+    internal class MainMenuControllerClass_method_45_Patch : ModulePatch
+    {
+        protected override MethodBase GetTargetMethod()
+        {
+            return typeof(MainMenuControllerClass).GetMethod(nameof(MainMenuControllerClass.method_45));
+        }
+
+        [PatchPrefix]
+        public static bool Prefix(MainMenuControllerClass __instance)
+        {
+            __instance.method_77();
+            return false;
+        }
+    }
+}

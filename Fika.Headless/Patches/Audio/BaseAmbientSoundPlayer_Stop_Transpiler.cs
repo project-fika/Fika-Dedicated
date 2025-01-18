3e82c946042f48682c11e77d7566cf6230b01d2b
Lacyway <20912169+Lacyway@users.noreply.github.com>
2025-01-18 15:08:27 +0100

    Fix nullref on Stop()

---
+++ Fika.Headless/Patches/Audio/BaseAmbientSoundPlayer_Stop_Transpiler.cs
@@ -0,0 +1,29 @@
+﻿using Audio.AmbientSubsystem;
+using HarmonyLib;
+using SPT.Reflection.Patching;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Reflection.Emit;
+
+namespace Fika.Headless.Patches.Audio
+{
+    internal class BaseAmbientSoundPlayer_Stop_Transpiler : ModulePatch
+    {
+        protected override MethodBase GetTargetMethod()
+        {
+            return typeof(BaseAmbientSoundPlayer).GetMethod(nameof(BaseAmbientSoundPlayer.Stop));
+        }
+
+        [PatchTranspiler]
+        public static IEnumerable<CodeInstruction> Transpile(IEnumerable<CodeInstruction> instructions)
+        {
+            // Create a new set of instructions
+            List<CodeInstruction> instructionsList =
+            [
+                new CodeInstruction(OpCodes.Ret) // Return immediately
+            ];
+
+            return instructionsList;
+        }
+    }
+}
